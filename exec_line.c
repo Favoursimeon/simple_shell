0efed220f6f4d99b62ab3c8f95b4310c69f4b13a
Favoursimeon <favoursimeon12w@gmail.com>
2023-09-19 20:01:40 +0000

    How to comeout of this

---
+++ exec_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * exec_line - finds builtins and commands
+ *
+ * @datash: data relevant (args)
+ * Return: 1 on success.
+ */
+int exec_line(d_sh *datash)
+{
+	int (*builtin)(d_sh *datash);
+
+	if (datash->args[0] == NULL)
+		return (1);
+
+	builtin = get_builtin(datash->args[0]);
+
+	if (builtin != NULL)
+		return (builtin(datash));
+
+	return (cmd_exec(datash));
+}
